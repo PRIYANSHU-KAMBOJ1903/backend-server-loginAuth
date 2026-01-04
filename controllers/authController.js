d4d022a68c0f5627151ba1c92e5ed22314ab91e3
Priyanshu <priyanshukamboj1903@gmail.com>
2026-01-04 17:58:34 +0530

    done change

--- controllers/authController.js
+++ controllers/authController.js
@@ -21,8 +21,8 @@ export const register=async (req,res)=>{
         const token=jwt.sign({id:user._id},process.env.JWT_SECRET,{expiresIn:'7d'});
         res.cookie('token',token,{
             httpOnly:true,
-            secure:process.env.NODE_ENV==='production',
-            sameSite:process.env.NODE_ENV==='production'?'none':'strict',
+            secure: true,
+            sameSite: "None",
             maxAge:7*24*60*60*1000,
         });
         //sending welcome email
@@ -56,8 +56,8 @@ export const login=async (req,res)=>{
         const token=jwt.sign({id:user._id},process.env.JWT_SECRET,{expiresIn:'7d'});
         res.cookie('token',token,{
             httpOnly:true,
-            secure:process.env.NODE_ENV==='production',
-            sameSite:process.env.NODE_ENV==='production'?'none':'strict',
+           secure: true,
+            sameSite: "None",
             maxAge:7*24*60*60*1000,
         })
         return res.json({success:true});
@@ -72,8 +72,8 @@ export const logout=async (req,res)=>{
     try{
         res.clearCookie('token',{
              httpOnly:true,
-            secure:process.env.NODE_ENV==='production',
-            sameSite:process.env.NODE_ENV==='production'?'none':'strict',
+           secure: true,
+             sameSite: "None",
         })
         return res.json({success:true,message:'Logged Out'})
     }catch(error){
--- server.js
+++ server.js
@@ -1,39 +1,39 @@
-import express from "express";
-import cors from "cors";
-import 'dotenv/config'
-import cookieParser from "cookie-parser";
-import connectDB from "./config/mongodb.js";
-import authRouter from './routes/authRouts.js'
-import userRouter from "./routes/userRouts.js";
-const app=express();
-const port=process.env.PORT || 4000;
+  import express from "express";
+  import cors from "cors";
+  import 'dotenv/config'
+  import cookieParser from "cookie-parser";
+  import connectDB from "./config/mongodb.js";
+  import authRouter from './routes/authRouts.js'
+  import userRouter from "./routes/userRouts.js";
+  const app=express();
+  const port=process.env.PORT || 4000;
 
-connectDB();
-const allowerdOrigins=['http://localhost:5173']
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-app.use(cookieParser());
-app.use(cors({
-  origin: function(origin, callback) {
-    if (!origin || allowerdOrigins.includes(origin)) {
-      callback(null, true)
-    } else {
-      callback(new Error("Not allowed by CORS"))
-    }
-  },
-  credentials: true // important for cookies
-}));
+  connectDB();
+  const allowerdOrigins=['http://localhost:5173']
+  app.use(express.json());
+  app.use(express.urlencoded({extended:true}));
+  app.use(cookieParser());
+  app.use(cors({
+    origin: function(origin, callback) {
+      if (!origin || allowerdOrigins.includes(origin)) {
+        callback(null, true)
+      } else {
+        callback(new Error("Not allowed by CORS"))
+      }
+    },
+    credentials: true // important for cookies
+  }));
 
 
-//API Endpoints
-app.get("/",(req,res)=>{
-    res.send("API is working fine");
-})
-app.use('/api/auth',authRouter);
-app.use('/api/user',userRouter);
-app.listen(port,(req,res)=>{
-    console.log(`Server Starting on Port:${port}`)
-})
+  //API Endpoints
+  app.get("/",(req,res)=>{
+      res.send("API is working fine");
+  })
+  app.use('/api/auth',authRouter);
+  app.use('/api/user',userRouter);
+  app.listen(port,(req,res)=>{
+      console.log(`Server Starting on Port:${port}`)
+  })
 
 
 
